dd31c3d1f76b5908046133e4f1951c6dbf5f809c
jkibilds <juris.kibilds@gmail.com>
2024-03-17 22:10:07 +0200

    fetchngs script and updated nf config to download fastqs

---
+++ src/fetch-reads.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#PBS -l walltime=96:00:00
+#PBS -l nodes=1:ppn=1,pmem=5g
+#PBS -q batch
+#PBS -j oe
+#PBS -o logs/$PBS_JOBNAME.$PBS_JOBID.log
+
+source activate nf
+module load singularity/3.11.4
+
+cd $PBS_O_WORKDIR #so that all paths are interpreted relative to project root
+
+JOB_BASENAME=$(echo $PBS_JOBNAME | cut -d . -f 1) # dots are not allowed in nf run names
+RUN_NAME="$JOB_BASENAME"-retry-06
+
+INPUT=data/metadata/metadata_w_accesions.tsv
+RUN_LIST=temp/run_list.tsv
+OUTDIR=data/raw/$JOB_BASENAME
+CONFIG_FILE=src/nf.config
+NF_WORKDIR=temp/nf_work_$JOB_BASENAME
+
+export NXF_SINGULARITY_CACHEDIR=bin/nf_singularity_cache
+
+# extract run accessions from metadata file
+cut -f 2 $INPUT | grep ERR* > $RUN_LIST
+
+# launch nf-core fetchngs pipeline
+nextflow run nf-core/fetchngs \
+	-r 1.12.0 \
+	-name $RUN_NAME \
+	-profile singularity \
+	-c $CONFIG_FILE \
+	-work-dir $NF_WORKDIR \
+	-resume \
+	-ansi-log false \
+	--input $RUN_LIST \
+	--download_method aspera \
+	--outdir $OUTDIR
+
+# clean the input file
+rm $RUN_LIST
